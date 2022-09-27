632729762b74dac4ddc9d1a64b0b3e92d63c3477
Arya <aryamol1015@gmail.com>
2022-09-27 16:28:09 +0530

    added default behaviour

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ const grid = document.getElementsByClassName('grid')[0];
 
 
 
-window.addEventListener('load',dayNightMode)
+window.addEventListener('load',dayNightMode);
+window.addEventListener('load',defaultSetting)
 input.addEventListener('keydown',(event)=>{
     if(event.key === "Enter")
     loadImg();
 })
 
 function loadImg(){
-    removeImages();
+    grid.innerHTML = ""
    
     const url = 'https://api.unsplash.com/search/photos/?query='+input.value+'&per_page&client_id=spXt3MjEwqgwfqgO7wIjvt2YUD7F9K8TDZjhADqTBTA'
      fetch(url).then((response)=>{
@@ -36,8 +37,29 @@ function loadImg(){
 
 }
 
-function removeImages(){
-    grid.innerHTML = " ";
+function defaultSetting(){
+    // grid.innerHTML = " ";
+    input.value = "car"
+    const url = 'https://api.unsplash.com/search/photos/?query=car&per_page&client_id=spXt3MjEwqgwfqgO7wIjvt2YUD7F9K8TDZjhADqTBTA'
+     fetch(url).then((response)=>{
+        if(response.ok){
+            return response.json();
+        }else{
+            alert(response.status)
+        }
+     }).then(data=>{
+         const imageNodes = [];
+         for(let i = 0 ;i<data.results.length ; i++){
+            imageNodes[i] = document.createElement('div');
+            imageNodes[i].className = 'img';
+            imageNodes[i].style.backgroundImage = 'url('+data.results[i].urls.raw+')';
+            imageNodes[i].addEventListener('dblclick',function(){
+                window.open(data.results[i].links.download,'_blank');
+
+            })
+            grid.appendChild(imageNodes[i]);
+         }
+     })
 }
 function dayNightMode(){
     const date = new Date();
